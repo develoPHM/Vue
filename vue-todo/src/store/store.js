f96a0accfbcc86ab39241672df7d0be54459577f
phmok1@naver.com <phmok1@naver.com>
2024-08-20 14:15:47 +0900

    리팩토링

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -22,7 +22,27 @@ const storage = {
 
 export const store = new Vuex.Store({
   state: {
-    // headerText : 'Todo it~~'
     todoItems: storage.fetch()
+  },
+  mutations : {
+    addOneItem(state, todoItem) {
+      console.log(state)
+      const obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      state.todoItems.push(obj)
+    },
+    removeOneItem(state, payload) {
+      localStorage.removeItem(payload.todoItem.item);
+      state.todoItems.splice(payload.index, 1);
+    },
+    toggleOneItem(state, payload) {
+      state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+      localStorage.removeItem(payload.todoItem.item)
+      localStorage.setItem(payload.todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems(state) {
+      localStorage.clear()
+      state.todoItems = []
+    }
   }
 })
