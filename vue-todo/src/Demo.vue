c97a406400078d5c87f48cf3d5d9883dd34c7de6
phmok1@naver.com <phmok1@naver.com>
2024-08-23 09:07:08 +0900

    .

---
+++ vue-todo/src/Demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{orginalPrice}}</p>
+    <p>{{doublePrice}}</p>
+    <p>{{triplePrice}}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['orginalPrice', 'doublePrice', 'triplePrice'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
