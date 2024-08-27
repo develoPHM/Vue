4ca7038cdd9be047a12f7d7e87c0c015da17af3f
phmok1@naver.com <phmok1@naver.com>
2024-08-27 17:45:21 +0900

    .

---
+++ vue-composition/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">버튼</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello'
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
