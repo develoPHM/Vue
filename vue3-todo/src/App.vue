d0bd7af3a0b1435d0e9df9858e6ff9721cec3253
phmok1@naver.com <phmok1@naver.com>
2024-08-28 13:17:14 +0900

    todoList 구현

---
+++ vue3-todo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <TodoHeader></TodoHeader>
+  <TodoInput></TodoInput>
+<!--  프롭스로 내려준다-->
+<!--<TodoList :하위프롭스이름="상위컴포넌트의 데이터이름" -->
+  <TodoList :todoItems="todoItems"></TodoList>
+</template>
+
+<script>
+import TodoHeader from '@/components/TodoHeader.vue';
+import TodoInput from '@/components/TodoInput.vue';
+import TodoList from '@/components/TodoList.vue';
+import {ref} from 'vue';
+
+export default {
+  components: {TodoInput, TodoHeader, TodoList},
+  setup() {
+    const todoItems = ref([])
+
+    function fetchTodos() {
+      const result = []
+      for (let i = 0; i < localStorage.length; i++ ) {
+        const todoItem = localStorage.key(i);
+        result.push(todoItem)
+        // items.value.push(todoItem)
+      }
+      return result
+    }
+    todoItems.value = fetchTodos();
+
+    return {todoItems}
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3-todo/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in todoItems" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<script>
+// import {ref} from 'vue';
+
+export default {
+  props: ['todoItems'],
+
+  // setup() {
+  //   const items = ref([]);
+  //
+  //   return {items}
+  // }
+}
+</script>
+
+<style scoped>
+
+</style>
