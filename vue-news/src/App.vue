a535920cc29dd987fb9dff77f9ffd0fb080d15b4
phmok1@naver.com <phmok1@naver.com>
2024-09-09 17:31:41 +0900

    라우터 생성

---
+++ vue-news/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ToolBar></ToolBar>
+    <routerView></routerView>
+
+  </div>
+</template>
+
+<script>
+import ToolBar from '@/components/ToolBar.vue';
+export default {
+  components: {
+    ToolBar,
+  }
+}
+</script>
+
+<style>
+</style>
