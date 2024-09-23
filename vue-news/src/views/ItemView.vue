50547173ab036bf616c62c388e4215babfd554cf
phmok1@naver.com <phmok1@naver.com>
2024-09-23 16:24:28 +0900

    ask 동적 라우트 매칭

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    <p> {{ fetchedItem.title}}</p>
-    <div>
-      {{ fetchedItem.content }}
-    </div>
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link v-bind:to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2> {{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content"></div>
+    </section>
   </div>
 </template>
 
@@ -23,5 +40,18 @@ export default {
 </script>
 
 <style scoped>
-
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.8rem;
+}
 </style>
