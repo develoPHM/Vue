c0e018df640c1b105146ccc4b5b8a0bf0df6b0a9
phmok1@naver.com <phmok1@naver.com>
2024-11-11 17:38:44 +0900

    .

---
+++ vue_ts/vue-todo/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <header>
+      <h1>.Vue todo with TS.</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem></TodoListItem>
+        </ul>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TodoInput from '@/components/TodoInput.vue';
+import TodoListItem from '@/components/TodoListItem.vue';
+
+const STORAGE_KEY = 'todo';
+const storage = {
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || '[]';
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
+
+export default Vue.extend({
+  components: { TodoListItem, TodoInput },
+  data() {
+    return {
+      todoText: '',
+      todoItems: [],
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = '';
+    },
+    fetchTodoItems() {
+      storage.fetch();
+    },
+  },
+  created() {
+    this.fetchTodoItems();
+  },
+});
+</script>
+
+<style scoped></style>
