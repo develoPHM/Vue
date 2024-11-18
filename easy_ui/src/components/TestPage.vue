df058de7ec1c8408dd8ede8b29370e775b6ea8cb
phmok1@naver.com <phmok1@naver.com>
2024-11-18 15:32:53 +0900

    .

---
+++ easy_ui/src/components/TestPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="{ active: isActive, 'text-large': isLarge }">This is a box.</div>
+  <button @click="toggleClass">Toggle Class</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const isActive = ref(false);
+    const isLarge = ref(true);
+
+    const toggleClass = () => {
+      isActive.value = !isActive.value;
+      isLarge.value = !isLarge.value;
+    };
+
+    return { isActive, isLarge, toggleClass };
+  },
+};
+</script>
+
+<style>
+.active {
+  color: green;
+}
+
+.text-large {
+  font-size: 24px;
+}
+</style>
