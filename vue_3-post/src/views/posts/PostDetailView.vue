1407c4aa9da3092719a0877d1ef9a4f6e284ae39
phmok1@naver.com <phmok1@naver.com>
2024-12-09 10:40:49 +0900

    버튼 라우터 설정

--- vue_3-post/src/views/posts/PostDetailView.vue
+++ vue_3-post/src/views/posts/PostDetailView.vue
@@ -1,13 +1,47 @@
 <template>
 	<div>
-		<h2>게시글 상세</h2>
+		<h2>제목</h2>
+		<p>내용</p>
+		<p class="tesxt-muted">2020-01-01</p>
 		<hr class="my-4" />
-		<p>params: {{ $route.params }}</p>
-		<p>query: {{ $route.query }}</p>
-		<p>hash: {{ $route.hash }}</p>
+		<div class="row g-2">
+			<div class="col-auto">
+				<buttton class="btn btn-outline-dark">이전글</buttton>
+			</div>
+			<div class="col-auto">
+				<buttton class="btn btn-outline-dark">다음글</buttton>
+			</div>
+			<div class="col-auto me-auto"></div>
+			<div class="col-auto">
+				<buttton class="btn btn-outline-dark" @click="goListPage">목록</buttton>
+			</div>
+			<div class="col-auto">
+				<buttton class="btn btn-outline-dark" @click="goEditPage">수정</buttton>
+			</div>
+			<div class="col-auto">
+				<buttton class="btn btn-outline-dark">삭제</buttton>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+const id = route.params.id;
+const goListPage = () => {
+	router.push({
+		name: 'PostList',
+	});
+};
+const goEditPage = () => {
+	router.push({
+		name: 'PostEdit',
+		params: { id },
+	});
+};
+</script>
 
 <style lang="scss" scoped></style>
