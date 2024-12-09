448042208184e53e2f7132001482582c2bac0604
phmok1@naver.com <phmok1@naver.com>
2024-12-09 10:49:18 +0900

    404 페이지 구현

---
+++ vue_3-post/src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="text-center py-5">
+		<h1>404 Not Found</h1>
+		<br />
+		<button class="btn btn-primary" @click="goHome">홈으로</button>
+	</div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goHome = () => {
+	router.push({
+		name: 'Home',
+	});
+};
+</script>
+
+<style lang="scss" scoped></style>
