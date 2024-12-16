99f252af491e35b0e4daca7e3f938c7a18cab102
phmok1@naver.com <phmok1@naver.com>
2024-12-16 10:02:24 +0900

    api 수정

--- vue_3-post/src/api/post.js
+++ vue_3-post/src/api/post.js
@@ -11,8 +11,12 @@ export function getPostById(id) {
 export function createPost(data) {
 	return posts.post('/', data);
 }
+// put은 전체데이터, patch는 일부데이터 수정임
+// export function updatePost(id, data) {
+// 	return posts.put(`/${id}`, data);
+// }
 export function updatePost(id, data) {
-	return posts.put(`/${id}`, data);
+	return posts.patch(`/${id}`, data);
 }
 export function deletePost(id) {
 	return posts.delete(`/${id}`);
