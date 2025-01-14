cd6c2ef760082d139e2eb3d2876c301cdbf84f7c
phmok1@naver.com <phmok1@naver.com>
2025-01-14 16:10:37 +0900

    .

--- vue_3-post/src/App.vue
+++ vue_3-post/src/App.vue
@@ -8,6 +8,7 @@
 import TheHeader from '@/components/layouts/TheHeader.vue';
 import TheView from '@/components/layouts/TheView.vue';
 import AppAlert from '@/components/app/AppAlert.vue';
+console.log('a')
 </script>
 
 <style lang="scss" scoped></style>
