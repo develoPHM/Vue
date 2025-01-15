372a4872a007902be582950cd48ebee5588e49bb
phmok1@naver.com <phmok1@naver.com>
2025-01-15 14:40:11 +0900

    초기설정

--- vue_nuxt/nuxt.config.ts
+++ vue_nuxt/nuxt.config.ts
@@ -1,7 +1,8 @@
-import { defineNuxtConfig } from 'nuxt/config';
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
     typeCheck: true,
   },
+  modules: ['nuxt-quasar-ui'],
 });
