0e09a8b79b8055d8ca5227fb00f0bb820cd1ebd0
phmok1@naver.com <phmok1@naver.com>
2025-01-15 17:18:19 +0900

    Nuxt 기본 컴포넌트 계층

---
+++ vue_nuxt/components/CourseCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-card class="my-card">
+    <img
+      src="https://tricolor-fern-8f6.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F0fd9cc4f-08a1-4b1d-bce3-e46c9d365c2d%2F5d66f274-a04e-4bc4-ad2d-fbb47ec63653%2F%25E1%2584%2582%25E1%2585%25B2%25E1%2584%258C%25E1%2585%25B5%25E1%2586%25AB%25E1%2584%2589%25E1%2585%25B3_%25E1%2584%2587%25E1%2585%25A2%25E1%2584%2580%25E1%2585%25A7%25E1%2586%25BC%25E1%2584%2592%25E1%2585%25AA%25E1%2584%2586%25E1%2585%25A7%25E1%2586%25AB_6.png?table=block&id=17c6b329-6319-80aa-ba68-d2c4544ced5c&spaceId=0fd9cc4f-08a1-4b1d-bce3-e46c9d365c2d&width=1420&userId=&cache=v2"
+      style="width: 400px"
+    />
+
+    <q-card-section>
+      <div class="text-h6 ellipsis">카드 제목입니다.</div>
+      <div class="text-subtitle2 ellipsis text-grey-8">
+        카드의 내용입니다. 카드의 내용입니다.
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts"></script>
