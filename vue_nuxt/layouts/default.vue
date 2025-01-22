c09436aa2254aed40192c0afca1bdea16fbb2263
phmok1@naver.com <phmok1@naver.com>
2025-01-22 16:07:12 +0900

    동적 레이아웃

--- vue_nuxt/layouts/default.vue
+++ vue_nuxt/layouts/default.vue
@@ -2,10 +2,19 @@
   <q-layout view="hHh lpR fFf" class="bg-grey-2">
     <q-header elevated class="bg-dark text-white">
       <q-toolbar>
-        <q-toolbar-title> Vue & Nuxt </q-toolbar-title>
-        <q-btn stretch flat label="Home" to="/" />
+        <q-toolbar-title>Vue & Nuxt Mastery Class</q-toolbar-title>
         <q-separator dark vertical />
-        <q-btn stretch flat label="About" to="/about" />
+        <NuxtLink v-slot="{ navigate }" custom to="/">
+          <q-btn stretch flat label="Home" no-caps @click="navigate()" />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/about">
+          <q-btn stretch flat label="About" no-caps @click="navigate()" />
+        </NuxtLink>
+        <q-separator dark vertical />
+        <NuxtLink v-slot="{ navigate }" custom to="/admin">
+          <q-btn stretch flat label="Admin" no-caps @click="navigate()" />
+        </NuxtLink>
       </q-toolbar>
     </q-header>
     <q-page-container :style="pageContainerStyle">
@@ -13,6 +22,7 @@
     </q-page-container>
   </q-layout>
 </template>
+
 <script setup lang="ts">
 const pageContainerStyle = computed(() => ({
   maxWidth: '1080px',
---
+++ vue_nuxt/pages/admin/dynamic-layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-my-xl text-center q-gutter-md">
+      <q-btn
+        label="default layout"
+        color="dark"
+        @click="changePageLayout('default')"
+      />
+      <q-btn
+        label="admin layout"
+        color="dark"
+        @click="changePageLayout('admin')"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { LayoutKey } from '#build/types/layouts';
+
+const changePageLayout = (layout: LayoutKey) => {
+  // 동적 레이아웃 변경
+  setPageLayout(layout);
+};
+definePageMeta({
+  // 레이아웃 적용하기 싫으면 false
+  layout: false,
+});
+</script>
+
+<style scoped></style>
