0887d0c3e127d9c12fb7f2d42388ed27dba6018b
phmok1@naver.com <phmok1@naver.com>
2025-01-24 10:18:20 +0900

    i18n

---
+++ vue_nuxt/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    shim: false,
+    typeCheck: true,
+  },
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
+  },
+  imports: {
+    presets: [
+      {
+        from: 'vue-i18n',
+        imports: ['useI18n'],
+      },
+    ],
+  },
+});
