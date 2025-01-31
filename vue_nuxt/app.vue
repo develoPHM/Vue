f86f92497a9a3b221ef9e265a0e63182f0015b6e
phmok1@naver.com <phmok1@naver.com>
2025-01-31 09:14:59 +0900

    .

---
+++ vue_nuxt/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+console.log('nuxtApp: ', nuxtApp);
+
+// vue application instance
+// nuxtApp.vueApp.component()
+// nuxtApp.vueApp.directive()
+// nuxtApp.vueApp.use()
+
+// provide 활용
+// nuxtApp.provide('hello', (msg: string) => `Hello ${msg}`);
+</script>
